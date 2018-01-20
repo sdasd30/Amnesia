13a8b0b150a8efea44920ab2b8aa1b1c94fd66ac
sdasd30 <danielzhao2@gmail.com>
2018-01-19 19:25:55 -0500

    Added camera follow and Tiled2Unity

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Follow : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    public float speed = 6.0f;
+    
+    void Update () {
+        float interpolation = speed * Time.deltaTime;
+        	
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interpolation);
+        
+        this.transform.position = position;
+    }
+}
---
+++ Assets/Tiled/Temp Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Temp Tiles" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../Art/TempTiles.png" width="96" height="32"/>
+</tileset>
